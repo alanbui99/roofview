7a2cb9cc59a02b1053851711b469e5b78c824763
Thang Bui <minhthang200399@gmail.com>
2021-04-07 02:37:22 -0400

    change hero

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,9 +1,17 @@
+const { lookup } = require('geoip-lite');
+
 const {getFeaturedRooftops} = require("../services/rooftops.service")
 exports.getHome = async (req, res, next) => {
     try {        
-        const rooftops = await getFeaturedRooftops()
-        const heroRooftop = rooftops[Math.floor(Math.random() * rooftops.length)];
-        return res.render("home", {rooftops, heroRooftop, home: true});   
+
+        const userIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log(userIp)
+        console.log(lookup(userIp))
+        
+
+        const featuredRooftops = await getFeaturedRooftops()
+        const heroRooftop = featuredRooftops[Math.floor(Math.random() * featuredRooftops.length)];
+        return res.render("home", {featuredRooftops, heroRooftop, home: true});   
     } catch (err) {
         console.log(err)
     }
